4e7a08dd7983b1bf88638918d78061a97cd541cb
Bruno Saint Clair <brunosaint@dcomp.ufs.br>
2024-10-03 10:12:14 -0300

    :recycle: refactor: mudança nas urls

--- src/api/urls.py
+++ src/api/urls.py
@@ -2,16 +2,26 @@ from django.urls import path
 from .views import * 
 
 urlpatterns = [
-    path('listar-compras/', listar_compras, name='listar_compras'),
-    path('adicionar-compra/', adicionar_compra, name='adicionar_compra'),
-    path('visualizar-compra/<int:id_compra>/', visualizar_compra, name='visualizar_compra'),
-    path('editar-compra/<int:id_compra>/', editar_compra, name='editar_compra'),
-    path('editar-compra/<int:id_compra>/<str:campo>/', editar_campo, name='editar_campo'),
-    path('apagar-compra/<int:id_compra>/', apagar_compra, name='apagar_compra'),
-    path('filtrar-compras/ano/<int:ano>/', filtrar_compras_ano, name='filtrar_compras_ano'),
-    path('filtrar-compras/mes/<int:mes>-<int:ano>/', filtrar_compras_mes, name='filtrar_compras_mes'),
-    path('filtrar-compras/<int:dia_inicio>-<int:mes_inicio>-<int:ano_inicio>/<int:dia_limite>-<int:mes_limite>-<int:ano_limite>/', filtrar_compras_periodo, name='filtrar_compras_periodo'),
-    path('filtrar-compras/metodo-pagamento/<str:metodo_pagamento>/', filtrar_metodo_pagamento, name='filtrar_metodo-pagamento'),
-    path('filtrar-compras/categoria/<str:categoria>/', filtrar_categoria, name='filtrar_categoria'),
-    path('filtrar-compras/utilidade/<str:utilidade>/', filtrar_utilidade, name='filtrar_utilidade'),
+    # GET
+    path('', listar_compras, name='listar_compras'),
+    path('<int:id_compra>/', visualizar_compra, name='visualizar_compra'),
+    path('filtrar/', listar_compras, name='listar_compras_sem_filtro'),
+    path('filtrar/ano/<int:ano>/', filtrar_compras_ano, name='filtrar_compras_ano'),
+    path('filtrar/mes/<int:mes>-<int:ano>/', filtrar_compras_mes, name='filtrar_compras_mes'),
+    path('filtrar/periodo/<int:dia_inicio>-<int:mes_inicio>-<int:ano_inicio>/<int:dia_limite>-<int:mes_limite>-<int:ano_limite>/', filtrar_compras_periodo, name='filtrar_compras_periodo'),
+    path('filtrar/metodo-pagamento/<str:metodo_pagamento>/', filtrar_metodo_pagamento, name='filtrar_metodo-pagamento'),
+    path('filtrar/categoria/<str:categoria>/', filtrar_categoria, name='filtrar_categoria'),
+    path('filtrar/utilidade/<str:utilidade>/', filtrar_utilidade, name='filtrar_utilidade'),
+
+    # POST
+    path('adicionar/', adicionar_compra, name='adicionar_compra'),
+
+    # DELETE
+    path('apagar/<int:id_compra>/', apagar_compra, name='apagar_compra'),
+
+    # PUT
+    path('editar/<int:id_compra>/', editar_compra, name='editar_compra'),
+
+    # PATCH
+    path('editar/<int:id_compra>/<str:campo>/', editar_campo, name='editar_campo'),
 ]
